e22f086d4ba93fdb263cf972f058aeaf3a8b2f4e
Dominic Fezzie <dfezzie@gmail.com>
2023-09-14 22:34:44 -0700

    Move some variables to globals

--- setupGithubSSH.py
+++ setupGithubSSH.py
@@ -6,14 +6,14 @@ from codecs import decode
 KEY_NAME_PREFIX = 'github'
 PRIVATE_KEY_NAME = KEY_NAME_PREFIX + '_ed25519'
 PUBLIC_KEY_NAME = PRIVATE_KEY_NAME + '.pub'
+DEFAULT_KEY_DIRECTORY_PATH = os.path.expanduser('~') + '/.ssh'
+PRIVATE_KEY_PATH = DEFAULT_KEY_DIRECTORY_PATH + '/' + PRIVATE_KEY_NAME
+PUBLIC_KEY_PATH = DEFAULT_KEY_DIRECTORY_PATH + '/' + PUBLIC_KEY_NAME
 
 def generate_ssh_private_key():
     return Ed25519PrivateKey.generate()
 
-def write_ssh_key_pair(private_key, key_directory_path):
-    private_key_path = key_directory_path + '/' + PRIVATE_KEY_NAME
-    public_key_path = key_directory_path + '/' + PUBLIC_KEY_NAME
-
+def write_ssh_key_pair(private_key):
     private_bytes = private_key.private_bytes(
         encoding=crypto_serialization.Encoding.PEM,
         format=crypto_serialization.PrivateFormat.OpenSSH,
@@ -24,12 +24,12 @@ def write_ssh_key_pair(private_key, key_directory_path):
         crypto_serialization.PublicFormat.OpenSSH,
     )
 
-    with open(private_key_path, 'wb') as private_key_file:
+    with open(PRIVATE_KEY_PATH, 'wb') as private_key_file:
         private_key_file.write(private_bytes)
-    with open(public_key_path, 'wb') as public_key_file:
+    with open(PUBLIC_KEY_PATH, 'wb') as public_key_file:
         public_key_file.write(public_bytes)
-    os.chmod(private_key_path, stat.S_IRWXU)
-    os.chmod(public_key_path, stat.S_IRWXU)
+    os.chmod(PRIVATE_KEY_PATH, stat.S_IRWXU)
+    os.chmod(PUBLIC_KEY_PATH, stat.S_IRWXU)
 
 def display_public_key(private_key):
     public_bytes = private_key.public_key().public_bytes(
@@ -45,10 +45,8 @@ def add_key_to_ssh_agent(private_key_path):
 
 
 def main():
-    key_directory_path = os.path.expanduser('~') + '/.ssh'
-    private_key_path = key_directory_path + '/' + PRIVATE_KEY_NAME
     # Check for existing script SSH key default SSH folder
-    with os.scandir(key_directory_path) as sshDir:
+    with os.scandir(DEFAULT_KEY_DIRECTORY_PATH) as sshDir:
         keyMatches = list(filter(lambda itr: PRIVATE_KEY_NAME in itr.path, sshDir))
     if keyMatches:
         # TODO: Add logic to present an option for displaying the existing key
@@ -56,8 +54,8 @@ def main():
         exit()
 
     private_key = generate_ssh_private_key()
-    write_ssh_key_pair(private_key, key_directory_path)
-    add_key_to_ssh_agent(private_key_path)
+    write_ssh_key_pair(private_key)
+    add_key_to_ssh_agent(PRIVATE_KEY_PATH)
     display_public_key(private_key)
     
     
